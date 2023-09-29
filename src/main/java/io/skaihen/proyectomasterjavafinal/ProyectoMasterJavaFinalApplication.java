ee45a4a6ecbc383673ba0132e3b6ba777268aa4d
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-09-29 13:03:43 +0200

    init

---
+++ src/main/java/io/skaihen/proyectomasterjavafinal/ProyectoMasterJavaFinalApplication.java
@@ -0,0 +1,13 @@
+package io.skaihen.proyectomasterjavafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMasterJavaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMasterJavaFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/skaihen/proyectomasterjavafinal/ProyectoMasterJavaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.skaihen.proyectomasterjavafinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMasterJavaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
