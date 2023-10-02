4ff8637de13a0291ce9a7e3ce4b5ba3f067ea51c
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-02 17:12:35 +0200

    Update data.sql

--- api-rest/src/main/resources/data.sql
+++ api-rest/src/main/resources/data.sql
@@ -9,12 +9,12 @@ CREATE TABLE pokemons (
 	hp INT,
 	attack INT,
 	defense INT,
-	specialAttack INT,
-	specialDefense INT,
+	special_attack INT,
+	special_defense INT,
 	speed INT,
 	PRIMARY KEY (id));
 
-INSERT INTO Pokemons (id, name, type1, type2, level, hp, attack, defense, specialAttack, specialDefense, speed) VALUES (1, 'Bulbasaur', 'planta', 'veneno', 1, 45, 49, 49, 65, 65, 45);
-INSERT INTO Pokemons (id, name, type1, type2, level, hp, attack, defense, specialAttack, specialDefense, speed) VALUES (4, 'Charmander', 'fuego', NULL, 1, 39, 52, 43, 60, 50, 65);
-INSERT INTO Pokemons (id, name, type1, type2, level, hp, attack, defense, specialAttack, specialDefense, speed) VALUES (7, 'Squirtle', 'agua', NULL, 1, 44, 48, 65, 50, 64, 43);
-INSERT INTO Pokemons (id, name, type1, type2, level, hp, attack, defense, specialAttack, specialDefense, speed) VALUES (25, 'Pikachu', 'electrico', NULL, 1, 35, 55, 40, 50, 50, 90);
+INSERT INTO Pokemons (id, name, type1, type2, level, hp, attack, defense, special_attack, special_defense, speed) VALUES (1, 'Bulbasaur', 'planta', 'veneno', 1, 45, 49, 49, 65, 65, 45);
+INSERT INTO Pokemons (id, name, type1, type2, level, hp, attack, defense, special_attack, special_defense, speed) VALUES (4, 'Charmander', 'fuego', NULL, 1, 39, 52, 43, 60, 50, 65);
+INSERT INTO Pokemons (id, name, type1, type2, level, hp, attack, defense, special_attack, special_defense, speed) VALUES (7, 'Squirtle', 'agua', NULL, 1, 44, 48, 65, 50, 64, 43);
+INSERT INTO Pokemons (id, name, type1, type2, level, hp, attack, defense, special_attack, special_defense, speed) VALUES (25, 'Pikachu', 'electrico', NULL, 1, 35, 55, 40, 50, 50, 90);
