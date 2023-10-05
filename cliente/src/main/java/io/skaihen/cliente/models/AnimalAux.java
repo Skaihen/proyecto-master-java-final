510c825ce57d21c154ea71cd826f39aeafac70ba
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-05 13:56:57 +0200

    Update AnimalAux.java

--- cliente/src/main/java/io/skaihen/cliente/models/AnimalAux.java
+++ cliente/src/main/java/io/skaihen/cliente/models/AnimalAux.java
@@ -1,5 +1,7 @@
 package io.skaihen.cliente.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,20 +9,19 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class AnimalAux {
+    @Id
     private int id;
     private String name;
-    private String type1;
-    private String type2;
-    private int level;
-    private int hp;
-    private int attack;
+    private int intelligence;
+    private int power;
     private int defense;
-    private int specialAttack;
-    private int specialDefense;
     private int speed;
+    private int hp;
+    private int stealth;
 
     public int calculateTotalStats() {
-        return hp + attack + defense + specialAttack + specialDefense + speed;
+        return intelligence + power + defense + speed + hp + stealth;
     }
 }
