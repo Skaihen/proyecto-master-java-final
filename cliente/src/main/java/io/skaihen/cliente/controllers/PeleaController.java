0b9e8f23ea07f9980c82f7b81b6cb9a72c332aa1
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-06 12:28:17 +0200

    javadoc

--- cliente/src/main/java/io/skaihen/cliente/controllers/PeleaController.java
+++ cliente/src/main/java/io/skaihen/cliente/controllers/PeleaController.java
@@ -21,31 +21,76 @@ public class PeleaController {
     @Autowired
     PeleaService peleaService;
 
+    /**
+     * La función "peleas" devuelve una lista de objetos "Pelea" en formato JSON.
+     *
+     * @return Se devuelve una lista de objetos Pelea.
+     */
     @GetMapping(value = "/peleas", produces = "application/json")
     public List<Pelea> peleas() {
         return peleaService.peleas();
     }
 
+    /**
+     * La función "buscarPelea" en el código Java recupera un objeto "Pelea" por su
+     * ID.
+     *
+     * @param idPelea El id de la pelea que deseas buscar.
+     * @return El método devuelve un objeto de tipo "Pelea".
+     */
     @GetMapping(value = "/pelea/{idPelea}", produces = "application/json")
     public Pelea buscarPelea(@PathVariable int idPelea) {
         return peleaService.buscarPelea(idPelea);
     }
 
+    /**
+     * La función `nuevaPelea` crea una nueva batalla entre un aliado y un enemigo
+     * en función de sus
+     * respectivos ID.
+     *
+     * @param idAliado  La identificación del aliado en la pelea.
+     * @param idEnemigo La identidad del enemigo en la lucha.
+     * @return El método devuelve un objeto de tipo "Pelea".
+     */
     @PostMapping(value = "/pelea/{idAliado}/{idEnemigo}", produces = "application/json")
     public Pelea nuevaPelea(@PathVariable int idAliado, @PathVariable int idEnemigo) {
         return peleaService.nuevaPelea(idAliado, idEnemigo);
     }
 
+    /**
+     * La función "actualizarPelea" actualiza un objeto "Pelea" mediante el servicio
+     * "peleaService".
+     *
+     * @param pelea El parámetro "pelea" es de tipo Pelea, que es el cuerpo de
+     *              solicitud del punto final
+     *              API. Se consume en formato "aplicación/json".
+     */
     @PutMapping(value = "/pelea", consumes = "application/json")
     public void actualizarPelea(@RequestBody Pelea pelea) {
         peleaService.actualizarPelea(pelea);
     }
 
+    /**
+     * Esta función Java elimina un objeto "Pelea" con el ID especificado y devuelve
+     * una lista de los
+     * objetos "Pelea" restantes.
+     *
+     * @param idPelea La identificación de la pelea que debe eliminarse.
+     * @return El método devuelve una lista de objetos Pelea.
+     */
     @DeleteMapping(value = "/pelea/{idPelea}", produces = "application/json")
     public List<Pelea> borrarPelea(@PathVariable int idPelea) {
         return peleaService.borrarPelea(idPelea);
     }
 
+    /**
+     * La función "buscarAnimalGanador" es una petición GET que recupera el animal
+     * ganador de una pelea
+     * basándose en el "idPelea" dado.
+     *
+     * @param idPelea El id de la pelea para buscar al animal ganador.
+     * @return Se devuelve un objeto de tipo AnimalAux.
+     */
     @GetMapping(value = "/ganador/{idPelea}", produces = "application/json")
     public AnimalAux buscarAnimalGanador(@PathVariable int idPelea) {
         return peleaService.buscarAnimalGanador(idPelea);
--- cliente/src/main/java/io/skaihen/cliente/services/PeleaServiceImpl.java
+++ cliente/src/main/java/io/skaihen/cliente/services/PeleaServiceImpl.java
@@ -20,16 +20,45 @@ public class PeleaServiceImpl implements PeleaService {
 
     private String url = "http://localhost:8080/";
 
+    /**
+     * La función "peleas()" devuelve una lista de todos los objetos "Pelea" del
+     * "peleaRepository".
+     *
+     * @return Se devuelve una lista de objetos Pelea.
+     */
     @Override
     public List<Pelea> peleas() {
         return peleaRepository.findAll();
     }
 
+    /**
+     * La función "buscarPelea" busca una pelea por su ID y devuelve una referencia
+     * a la misma.
+     *
+     * @param idPelea El parámetro "idPelea" es un número entero que representa el
+     *                identificador único de
+     *                un objeto "Pelea".
+     * @return El método devuelve un objeto de tipo "Pelea".
+     */
     @Override
     public Pelea buscarPelea(int idPelea) {
         return peleaRepository.getReferenceById(idPelea);
     }
 
+    /**
+     * La función `nuevaPelea` crea una nueva batalla entre dos animales, calcula
+     * las estadísticas totales
+     * de cada animal, determina el resultado de la batalla (victoria, derrota o
+     * empate), guarda el
+     * registro de la batalla en la base de datos y devuelve el objeto de la
+     * batalla.
+     *
+     * @param idAliado  La identificación del animal aliado en la pelea.
+     * @param idEnemigo El parámetro "idEnemigo" representa la identificación del
+     *                  animal enemigo en una
+     *                  batalla.
+     * @return El método devuelve un objeto de tipo "Pelea".
+     */
     @Override
     public Pelea nuevaPelea(int idAliado, int idEnemigo) {
         AnimalAux aliado = template.getForObject(url + "animal/" + idAliado, AnimalAux.class);
@@ -52,17 +81,40 @@ public class PeleaServiceImpl implements PeleaService {
         return pelea;
     }
 
+    /**
+     * La función "actualizarPelea" actualiza un objeto "Pelea" en el
+     * "peleaRepository".
+     *
+     * @param pelea El parámetro "pelea" es un objeto de tipo "Pelea".
+     */
     @Override
     public void actualizarPelea(Pelea pelea) {
         peleaRepository.save(pelea);
     }
 
+    /**
+     * La función elimina una pelea con una ID determinada y devuelve una lista de
+     * todas las peleas
+     * restantes.
+     *
+     * @param idPelea El parámetro `idPelea` es un número entero que representa el
+     *                ID de la pelea que debe
+     *                eliminarse.
+     * @return El método devuelve una lista de objetos Pelea.
+     */
     @Override
     public List<Pelea> borrarPelea(int idPelea) {
         peleaRepository.deleteById(idPelea);
         return peleaRepository.findAll();
     }
 
+    /**
+     * La función busca el animal ganador de una pelea por su ID y lo devuelve.
+     *
+     * @param idPelea El id de la pelea para la que queremos encontrar el animal
+     *                ganador.
+     * @return El método devuelve un objeto de tipo AnimalAux.
+     */
     @Override
     public AnimalAux buscarAnimalGanador(int idPelea) {
         AnimalAux animalGanador = template.getForObject(
